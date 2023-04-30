44c600889db473f5833b1ec2270a92ab4ffdfb61
erratbi <yerratbi@gmail.com>
2023-04-30 13:59:45 +0100

    Added the CLI logic

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readline from 'readline'
-import { sleep, TITLE } from './lib/utils.js'
+import { readInput, TITLE } from './lib/utils.js'
+import ChatBot from './lib/ChatBot.js'
 
 let retries = 5
 
@@ -8,27 +9,53 @@ const rl = readline.createInterface({
   output: process.stdout,
 })
 
+const bot = new ChatBot()
+
 while (true) {
   try {
     console.clear()
     console.log(TITLE)
-
     console.log('Connecting...\n\n')
-    await sleep(5000)
-    throw new Error('Failed to connect')
+
+    await bot.init()
+
+    console.clear()
+    console.log(TITLE + '\n\n')
+
+    while (true) {
+      let prompt = await readInput(rl)
+
+      if (prompt.trim() === ':quit') {
+        bot.close()
+        break
+      } else if (prompt.trim() === ':reset') {
+        console.clear()
+        console.log(TITLE + '\n\n')
+        await bot.reset()
+        continue
+      }
+
+      let index = 0
+      process.stdout.write('\n\n🤖 : ')
+      await bot.askSync(prompt, (res) => {
+        process.stdout.write(`${res.substring(index)}`)
+        index = res.length
+      })
+      index = 0
+      process.stdout.write('\n\n')
+    }
   } catch {
     if (retries-- > 0) {
       console.clear()
       console.log(TITLE)
 
       console.log('Failed to connect, retrying...')
-      await sleep(5000)
     } else {
       console.clear()
       console.log(TITLE)
 
       console.log('Failed to connect, Abort.')
-      process.exit(0)
+      break
     }
   }
 }
